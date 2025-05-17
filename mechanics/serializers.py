c641e4eed0bdb052afcd092fbfeda1382d2832b0
Igor Wlodarczyk <wlodar.igor@gmail.com>
2025-05-17 18:37:21 +0200

    update

--- mechanics/serializers.py
+++ mechanics/serializers.py
@@ -7,5 +7,5 @@ class MechanicSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:
         model = Mechanic
-        fields = ['id', 'user', 'name', 'address', 'city', 'rating', 'created_at']
+        fields = ['id', 'user', 'name', 'address', 'description', 'contact_phone', 'city', 'rating', 'created_at']
         read_only_fields = ['id', 'user', 'rating', 'created_at']
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView, CustomTokenObtainPairView, ProfileUpdateView, ChangePasswordView, MeView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # <-- updated
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns += [
+    path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('password/change/', ChangePasswordView.as_view(), name='password_change'),
+]
+
+urlpatterns += [
+    path('me/', MeView.as_view(), name='user_detail'),
 ]

eee48f4db3f85a5c94ac1f879668970e6a9f39df
Igor Wlodarczyk <wlodar.igor@gmail.com>
2025-05-17 17:43:17 +0200

    added endpoint for list of mechanics

--- mechanics/views.py
+++ mechanics/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Mechanic
 from .serializers import MechanicSerializer
-from users.permissions import IsMechanic
+from users.permissions import IsMechanic, IsClient
 
 
 class MechanicProfileView(generics.RetrieveUpdateAPIView):
@@ -10,3 +10,9 @@ class MechanicProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user.mechanic_profile
+
+
+class MechanicListView(generics.ListAPIView):
+    queryset = Mechanic.objects.all()
+    serializer_class = MechanicSerializer
+    permission_classes = [permissions.AllowAny]

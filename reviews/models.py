92824da811614bb911feeabd8e56f49d648b861d
nielogicznie <254292@student.pwr.edu.pl>
2025-05-19 23:08:01 +0200

    changed note range to 1-5 integer only in review app

--- reviews/models.py
+++ reviews/models.py
@@ -9,7 +9,7 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     review_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # unikalny identyfikator
-    note = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    note = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     content = models.TextField(default='', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)  # data utworzenia
     updated_at = models.DateTimeField(auto_now=True)  # data aktualizacji
